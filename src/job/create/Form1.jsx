fe16a22cb8d99a8e8607ba49473adc2b5545f66b
Rishav Kumar <rishav74492644@gmail.com>
2023-08-11 19:27:33 +0530

    Add custom colors.

--- src/job/create/Form1.jsx
+++ src/job/create/Form1.jsx
@@ -48,7 +48,7 @@ const Form1 = (props) => {
       <div className="flex justify-end m-8 mt-20">
         <button
           type="button"
-          className="inline-flex w-full justify-center rounded-md bg-blue-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+          className="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
           onClick={() => setFormIndex(2)}
         >
           Next
