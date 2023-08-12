679f00dff59a3be3cd753a58f0c7f30efd06905e
Rishav Kumar <rishav74492644@gmail.com>
2023-08-12 17:49:58 +0530

    Remove mock data. (#14)

--- src/constants/job.js
+++ src/constants/job.js
@@ -1,15 +1,15 @@
 export const BASE_URL = "https://64d3618267b2662bf3dc1ea1.mockapi.io/jobs";
 
 export const FORM_DATA = {
-  applyType: "quick",
-  companyName: "Great Vibes",
-  industry: "Information Technology",
-  location: "Chennai, Tamil Nadu, India",
-  maxExperience: 2,
-  maxSalary: 50000,
-  minExperience: 1,
-  minSalary: 30000,
-  remoteType: "In-office",
+  applyType: "",
+  companyName: "",
+  industry: "",
+  location: "",
+  maxExperience: null,
+  maxSalary: null,
+  minExperience: null,
+  minSalary: null,
+  remoteType: "",
   title: "",
-  totalEmployee: "51-200",
+  totalEmployee: "",
 };
