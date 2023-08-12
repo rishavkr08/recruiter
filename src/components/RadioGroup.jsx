efcc80914f35c9c8c32a925f547d2e6f6ba65113
Rishav Kumar <rishav74492644@gmail.com>
2023-08-12 08:08:48 +0530

    Wire job create API with the form.

--- src/components/RadioGroup.jsx
+++ src/components/RadioGroup.jsx
@@ -9,6 +9,7 @@ const RadioGroup = (props) => {
       ...options.map((option, index) => ({
         label: option.label,
         checked: index === i ? checked : !checked,
+        value: option.value,
       })),
     ]);
   };
@@ -17,12 +18,13 @@ const RadioGroup = (props) => {
     <div className="flex flex-col">
       <div className="flex text-gray-900 text-sm font-medium mb-2">
         {label}
-        {required && <div className="text-red-500">*</div>}
+        {required && <div className="text-error">*</div>}
       </div>
       <div className={"flex " + (!!verticalOrder ? "flex-col" : "flex-row")}>
         {options.map((option, index) => {
           return (
             <RadioButton
+              key={index}
               label={option.label}
               wrapperClasses={!!verticalOrder ? "mb-4" : "mr-4"}
               checked={option.checked}
--- src/job/create/Form2.jsx
+++ src/job/create/Form2.jsx
@@ -1,13 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "../../components/TextField";
 import RadioGroup from "../../components/RadioGroup";
+import jobsApi from "../../apis/job";
 
 const Form2 = (props) => {
-  const { setFormIndex } = props;
+  const { formData, setFormData, setOpen } = props;
+  const [isLoading, setIsLoading] = useState(false);
   const [applyTypeOptions, setApplyTypeOptions] = useState([
-    { label: "Quick apply", checked: false },
-    { label: "External apply", checked: false },
+    {
+      label: "Quick apply",
+      checked: formData.applyType === "quick",
+      value: "quick",
+    },
+    {
+      label: "External apply",
+      checked: formData.applyType === "external",
+      value: "external",
+    },
   ]);
+
+  const setData = (data, name) => {
+    let tempFormData = { ...formData };
+    tempFormData[name] = data;
+    setFormData(tempFormData);
+  };
+
+  useEffect(() => {
+    console.log("applyTypeOptions - ", applyTypeOptions);
+    setData(applyTypeOptions.find((item) => item.checked)?.value, "applyType");
+  }, [applyTypeOptions]);
+
+  const saveData = async () => {
+    try {
+      setIsLoading(true);
+      await jobsApi.create(formData);
+      setOpen(false);
+    } catch (e) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="m-8">
@@ -19,34 +52,58 @@ const Form2 = (props) => {
           <div className="flex-1">
             <TextField
               label="Experience"
+              data={formData.minExperience}
+              name="minExperience"
               placeholder="Minimum"
+              setData={setData}
               wrapperClasses="mb-6 mr-6"
             />
           </div>
           <div className="flex-1">
-            <TextField placeholder="Maximum" wrapperClasses="mb-6 mt-5" />
+            <TextField
+              data={formData.maxExperience}
+              name="maxExperience"
+              placeholder="Maximum"
+              setData={setData}
+              wrapperClasses="mb-6 mt-5"
+            />
           </div>
         </div>
         <div className="flex justify-between">
           <div className="flex-1">
             <TextField
               label="Salary"
+              data={formData.minSalary}
+              name="minSalary"
               placeholder="Minimum"
+              setData={setData}
               wrapperClasses="mb-6 mr-6"
             />
           </div>
           <div className="flex-1">
-            <TextField placeholder="Maximum" wrapperClasses="mb-6 mt-5" />
+            <TextField
+              data={formData.maxSalary}
+              name="maxSalary"
+              placeholder="Maximum"
+              setData={setData}
+              wrapperClasses="mb-6 mt-5"
+            />
           </div>
         </div>
         <TextField
           label="Total employee"
+          data={formData.totalEmployee}
+          name="totalEmployee"
           placeholder="ex. 100"
+          setData={setData}
           wrapperClasses="mb-6"
         />
         <RadioGroup
           label="Apply type"
+          data={formData.applyType}
+          name="applyType"
           options={applyTypeOptions}
+          setData={setData}
           setOptions={setApplyTypeOptions}
         />
       </div>
@@ -54,9 +111,10 @@ const Form2 = (props) => {
         <button
           type="button"
           className="inline-flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
-          onClick={() => setFormIndex(1)}
+          onClick={saveData}
+          disabled={isLoading}
         >
-          Save
+          {isLoading ? "Saving..." : "Save"}
         </button>
       </div>
     </React.Fragment>
