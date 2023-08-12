938956e46b4e6fb9b1f165120f1142d82345a21a
Rishav Kumar <rishav74492644@gmail.com>
2023-08-12 08:12:55 +0530

    Add data handler in the TextField component.

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,15 +1,39 @@
 const TextField = (props) => {
-  const { disabled, label, placeholder, required, wrapperClasses } = props;
+  const {
+    data,
+    disabled,
+    error,
+    name,
+    label,
+    placeholder,
+    required,
+    setData,
+    wrapperClasses,
+  } = props;
+
+  const changeDateHandler = (event) => {
+    setData(event.target.value, name)
+  }
+
   return (
     <div className={"flex flex-col " + wrapperClasses}>
-			<div className="flex text-gray-900 text-sm font-medium mb-1">{label}{required && <div className="text-red-500">*</div>}</div>
+      <div className="flex text-gray-900 text-sm font-medium mb-1">
+        {label}
+        {required && <div className="text-error">*</div>}
+      </div>
       <input
-        class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder={placeholder}
+        className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-gray-dark"
         disabled={disabled}
+        name={name}
+        onChange={changeDateHandler}
+        placeholder={placeholder}
+        type="text"
+        value={data}
         required
       />
+      {error && (
+        <div className="flex text-error text-xs font-light mb-1">{error}</div>
+      )}
     </div>
   );
 };
